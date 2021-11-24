be5db383e531e12298f25d7b70d9888e3758f0d3
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-24 22:09:25 +0530

    added

--- jenkinsdeploy.groovy
+++ jenkinsdeploy.groovy
@@ -20,7 +20,7 @@ pipeline{
                 sh """
                 
                 scp -i /tmp/mine.pem hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/tmp
-                ssh -i /tmp/mine.pem ec2-user@${SERVER_IP} "sudo cp /tmp/mine.pem hello-${BUILD_NUM}.war /var/lib/tomcat/webapps"
+                ssh -i /tmp/mine.pem ec2-user@${SERVER_IP} "sudo cp /tmp/hello-${BUILD_NUM}.war /var/lib/tomcat/webapps"
                  
                 """
 
