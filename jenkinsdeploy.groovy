06253836f4e7fece9663df90ea66d695e32d157c
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-24 20:19:59 +0530

    deploy

---
+++ jenkinsdeploy.groovy
@@ -0,0 +1,28 @@
+//declarative pipeline
+pipeline{
+    agent any
+    parameters {
+        string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'type branch name here')
+        string(name: 'SERVER_IP', defaultValue: '', description: 'type SERVER_IP name here')
+        string(name: 'BUILD_NUM', defaultValue: '', description: 'type BUILD_NUM name here')
+    }
+    stages{
+        stage("download artifacts"){
+            steps{
+                sh """
+                aws s3 ls://chaituart
+                aws s3 cp s3://chaituart/hello-${BUILD_NUM}.war .
+                """
+            }
+        }
+        stage("copy artifacts to remote server"){
+            steps{
+                sh """
+                ssh -i /tmp/mine.pem ec2-user@${SERVER_IP} "systemctl status tomcat"
+                scp -i /tmp/mine.pem hello-${BUILD_NUM}.war ec2-user@${SERVER_IP}:/tmp
+                """
+
+            }
+        }
+    }
+}
