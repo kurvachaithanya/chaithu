6b2af97b97699cc0ea9a3cffc8c20069eb18fb24
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-27 00:20:26 +0530

    multiple10

--- jenkinsmultiple.groovy
+++ jenkinsmultiple.groovy
@@ -16,6 +16,7 @@ pipeline{
                 do 
                 echo $ip
                  scp -o strictHostkeychecking=no -i /tmp/mine.pem hello-${BUILD_NUMBER}.war ec2-user@$ip:/var/lib/tomcat/webapps
+                 ssh -o strictHostkeychecking=no -i /tmp/mine.pem ec2-user@$ip "hostname -i"
                 done          
                 '''
             }
