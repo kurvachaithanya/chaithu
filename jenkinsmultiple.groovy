ba79884ca1045327de4e6db34a17b5cd2757aad3
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-26 22:34:10 +0530

    multiple3

--- jenkinsmultiple.groovy
+++ jenkinsmultiple.groovy
@@ -2,7 +2,7 @@
 pipeline{
     agent any
     parameters {
-    string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'type branch name here')
+    //string(name: 'BRANCH_NAME', defaultValue: 'master', description: 'type branch name here')
         string(name: 'SERVERIPS', defaultValue: '', description: 'type SERVER_IP nme here')
         string(name: 'BUILD_NUM', defaultValue: '', description: 'type BUILD_NUM name here')
     }
@@ -10,7 +10,7 @@ pipeline{
         stage("multiple servers"){
             steps{
                 sh '''
-                aws s3 cp s3://chaituart/${BRANCH_NAME}/${BUILD_NUM}/hello-${BUILD_NUM}.war .
+                aws s3 cp s3://chaituart/hello-${BUILD_NUM}.war .
                 IFS=',' read -ra ADDR << "${SERVERIPS}"
                 for ip in \"${ADDR[@]}\";
                 do 
