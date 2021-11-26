cff2da5a7628b7b107f2895827fccd08abcd6c1c
K CHaitu <kchaithanya.2010@gmail.com>
2021-11-26 23:44:35 +0530

    multiple6

--- jenkinsmultiple.groovy
+++ jenkinsmultiple.groovy
@@ -3,19 +3,19 @@ pipeline{
     agent any
     parameters {
         string(name: 'SERVERIPS', defaultValue: '', description: 'type SERVER_IP nme here')
-        string(name: 'BUILD_NUM', defaultValue: '', description: 'type BUILD_NUM name here')
+        string(name: 'BUILD_NUMBER', defaultValue: '', description: 'type BUILD_NUM name here')
     }
     stages{
         stage("multiple servers"){
             steps{
                 sh '''
-                aws s3 cp s3://chaituart/hello-${BUILD_NUM}.war .
+                aws s3 cp s3://chaituart/hello-${BUILD_NUMBER}.war .
                 ls -l
                 IFS=',' read -ra ADDR <<< "${SERVERIPS}"
                 for ip in \"${ADDR[@]}\";
                 do 
                 echo $ip
-                 scp -o StrictHostkeychecking=no -i /tmp/mine.pem hello-${BUILD_NUM}.war ec2-user@$ip:/var/lib/tomcat/webapps
+                 scp -o StrictHostkeychecking=no -i /tmp/mine.pem hello-${BUILD_NUMBER}.war ec2-user@$ip:/var/lib/tomcat/webapps
                  ssh -o StrictHostkeychecking=no -i /tmp/mine.pem ec2-user@$ip "hostname"
                   # process "$i" 
                 done          
